563bc3c0f152176c1acd57cf69ab921daffb409d
victorhvn\VictorHugo <na__ruto96@hotmail.com>
2018-04-10 09:51:40 -0500

    primer commit
    proyecto de catologo, ya tiene recicly view

---
+++ catalogo/app/src/main/java/mx/com/victorhugo/catalogo/AdapterDatos.java
@@ -0,0 +1,52 @@
+package mx.com.victorhugo.catalogo;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class AdapterDatos extends RecyclerView.Adapter<AdapterDatos.ViewHolderDatos> {
+
+    ArrayList<String>listDatos;
+
+    public AdapterDatos(ArrayList<String> listDatos) {
+        this.listDatos = listDatos;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolderDatos onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,null,false);
+        return new ViewHolderDatos(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolderDatos holder, int position) {
+        holder.asignarDatos(listDatos.get(position));
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return listDatos.size();
+    }
+
+    public class ViewHolderDatos extends RecyclerView.ViewHolder{
+
+        TextView dato;
+
+        public ViewHolderDatos(View itemView) {
+            super(itemView);
+            dato =(TextView) itemView.findViewById(R.id.idDato);
+        }
+
+        public void asignarDatos(String datos) {
+            dato.setText(datos);
+        }
+    }
+
+}
